6b7a78cce022004e6f68aa5c69350b6d6c295763
Adrian <142854436+withyso@users.noreply.github.com>
2023-10-12 04:23:10 +0000

    cambio antes de empezar a inventar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello world");
+
+  let pronoun = ["the", "our", "their", "your"];
+  let adj = ["great", "big", "small", "tall", "fast", "fat"];
+  let nouns = ["racoon", "apple", "orange", "cat", "dog", "burger"];
+  let domain = [".com", ".es", ".gob", ".net"];
+
+  for (let i = 0; i < 4; i++) {
+    console.log(`${pronoun[i]}` + `${adj[i]}` + `${nouns[i]}` + `${domain[i]}`);
+  }
+
+  let randomWord = element => {
+    let randomIndex = Math.floor(Math.random() * element.length);
+    return element[randomIndex];
+  };
+
+  for (let i = 0; i < 15; i++) {
+    console.log(
+      `${randomWord(pronoun)}` +
+        `${randomWord(adj)}` +
+        `${randomWord(nouns)}` +
+        `${randomWord(domain)}`
+    );
+  }
 };
