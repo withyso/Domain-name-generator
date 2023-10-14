5f2fda477a94dd28a0b064a87cc17264aa9eff7f
Adrian <142854436+withyso@users.noreply.github.com>
2023-10-14 03:10:47 +0000

    Cambio de legibilidad en el for loop

--- src/app.js
+++ src/app.js
@@ -9,23 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello world");
 
-  let pronoun = ["the", "our", "your"];
-  let adj = ["great", "big", "small"];
+  let pronouns = ["the", "our", "your"];
+  let adjectives = ["great", "big", "small"];
   let nouns = ["racoon", "apple", "cat"];
-  let domain = [".com", ".net"];
+  let domains = [".com", ".net"];
 
   function iteracionesDeArreglos(arr1, arr2, arr3, arr4) {
-    for (let arr0Index = 0; arr0Index < arr1.length; arr0Index++) {
-      for (let arr2Index = 0; arr2Index < arr2.length; arr2Index++) {
-        for (let arr3Index = 0; arr3Index < arr3.length; arr3Index++) {
-          for (let arr4Index = 0; arr4Index < arr4.length; arr4Index++)
-            console.log(
-              `${arr1[arr0Index]} ${arr2[arr2Index]} ${arr3[arr3Index]} ${arr4[arr4Index]}`
-            );
+    for (let value1 of arr1) {
+      for (let value2 of arr2) {
+        for (let value3 of arr3) {
+          for (let value4 of arr4)
+            console.log(value1 + value2 + value3 + value4);
         }
       }
     }
   }
 
-  iteracionesDeArreglos(pronoun, adj, nouns, domain);
+  iteracionesDeArreglos(pronouns, adjectives, nouns, domains);
 };
